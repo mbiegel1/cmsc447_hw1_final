8797a8dae6b75d4b6c8dd11541c11dee1318f056
Mark Biegel <mbiegel1@umbc.edu>
2022-02-28 18:05:25 -0500

    Changing crud .sh

--- crud_app.sh
+++ crud_app.sh
@@ -1,6 +1,3 @@
-source src/flask/bin/activate
 cd src
-sudo apt-get update
-sudo apt-get -y install python3-pip
-pip install Flask
+pip install -r requirements.txt
 python3 flask_backend.py
---
+++ crud_app_backup.sh
@@ -0,0 +1,6 @@
+source src/flask/bin/activate
+cd src
+sudo apt-get update
+sudo apt-get -y install python3-pip
+pip install Flask
+python3 flask_backend.py
